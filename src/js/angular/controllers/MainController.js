3537b9fef6ad9f6ec35f5cb6e5812e44513e296b
Julian Labuschagne <julian@kuberfirma.com>
2015-11-22 13:13:24 +0200

    Pull image data from Instagram

--- src/js/angular/controllers/MainController.js
+++ src/js/angular/controllers/MainController.js
@@ -1,3 +1,16 @@
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	$scope.title = 'A Title set in the controller';
+	
+	var url = 'https://api.instagram.com/v1/users/36494182/media/recent/?callback=JSON_CALLBACK&client_id=e14d5cc0f5ab4001959555ced2ee34e6'; 
+	$http.jsonp(url)
+		.success(function(data) {
+			console.log(data);
+			$scope.images = data.data;
+			
+			console.log($scope.images);
+		})
+		.error(function(data) {
+			$scope.data = "Request failed";
+			console.log($scope.data);
+		});
 }]);
