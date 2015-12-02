f8f06d875a63918b3c112f11f6da3382dde0d53f
Julian Labuschagne <julian@kuberfirma.com>
2015-12-02 11:47:14 +0200

    Modifiy Gruntfile.js to use grunt watch, connect and hugo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,6 @@
 // Gruntfile.js
-
-// our wrapper function (required by grunt and its plugins)
-// all configuration goes inside this function
 module.exports = function (grunt) {
-	// Configure GRUNT
+	
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		
@@ -12,50 +9,67 @@ module.exports = function (grunt) {
 				options: {
 					compress: true
 				},
-				files: {'dist/css/app.css': 'src/css/app.less'}
-			}
-		},
-		watch: {
-			files: ['src/css/*.less'],
-			tasks: ['less']
-		},
-		concat: {
-			js: {
-				src: [
-					'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js',
-					'src/js/jquery/magnificient_popups.js',
-					'src/js/angular/app.js', 
-					'src/js/angular/controllers/**.js', 
-					'src/js/angular/filters/**.js', 
-				],
-				dest: 'dist/js/app.js'
-			},
-			css: {
-				src: ['bower_components/magnific-popup/dist/magnific-popup.css', 'dist/css/app.css'],
-				dest: 'dist/css/app.css'
-			}
-		},
-		serve: {
-			options: {
-				port: 9000,
-				'aliases': {
-					'test.js': {
-						tasks: ['less', 'concat'],
-						output: 'index.html'
-					}
+				files: {
+					'themes/franson/static/assets/css/styles.css':'src/less/app.less'
 				}
 			}
-		}
-
+		},
+		
+		exec: {
+			hugo: 'hugo -v'
+		},
+		
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					hostname: '*',
+					base: 'public',
+					open: true,
+					livereload: true
+				}
+			}
+		},
+		
+		watch: {
+			src: {
+				files: ['src/less/*.less'],
+				tasks: ['less', 'exec'],
+				options: {
+					livereload: true
+				}
+			},
+			layouts: {
+				files: ['themes/franson/layouts/**/**.*'],
+				tasks: ['exec'],
+				options: {
+					livereload: true
+				}
+			},
+			content: {
+				files: ['content/**/**.*'],
+				tasks: ['exec'],
+				options: {
+					livereload: true
+				}
+			},
+			config: {
+				files: ['config.toml'],
+				tasks: ['exec'],
+				options: {
+					livereload: true
+				}
+			}
+			
+		},
+		
 	});
-
-	// Load Grunt Plugins
+	
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-serve');
-
-	// Create tasks
-	grunt.registerTask('default', ['less', 'concat']);
+	grunt.loadNpmTasks('grunt-exec');
+	
+	grunt.registerTask('default', ['connect', 'watch']);
+	
 };
