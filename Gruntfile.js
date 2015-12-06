f9c0aef76e0cd891850ab8d811a39fa561f54792
Julian Labuschagne <julian@kuberfirma.com>
2015-12-06 16:22:19 +0200

    Use variable for theme dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,11 @@
 module.exports = function (grunt) {
 	
 	grunt.initConfig({
+		//
+		// Change this to the hugo theme name you are using
+		//
+		hugo_theme: 'franson',
+		
 		pkg: grunt.file.readJSON('package.json'),
 		
 		less: {
@@ -10,8 +15,26 @@ module.exports = function (grunt) {
 					compress: true
 				},
 				files: {
-					'themes/franson/static/assets/css/styles.css':'src/less/app.less'
+					'themes/<%= hugo_theme %>/static/assets/css/styles.css':'src/less/app.less'
 				}
+			}
+		},
+		
+		concat: {
+			js: {
+				src: [
+					'bower_components/jquery/dist/jquery.min.js',
+					'bower_components/bootstrap/dist/js/bootstrap.min.js',
+					'bower_components/magnific-popup/dist/jquery.magnific-popup.min.js'
+				],
+				dest: 'themes/<%= hugo_theme %>/static/assets/js/app.js'
+			},
+			css: {
+				src: [
+					'bower_components/magnific-popup/dist/magnific-popup.css', 
+					'themes/<%= hugo_theme %>/static/assets/css/styles.css'
+				],
+				dest: 'themes/<%= hugo_theme %>/static/assets/css/styles.css'
 			}
 		},
 		
@@ -32,15 +55,29 @@ module.exports = function (grunt) {
 		},
 		
 		watch: {
-			src: {
+			less: {
 				files: ['src/less/*.less'],
 				tasks: ['less', 'exec'],
 				options: {
 					livereload: true
 				}
 			},
+			js: {
+				files: ['src/js/**/**.js'],
+				tasks: ['concat', 'exec'],
+				options: {
+					livereload: true
+				}
+			},
 			layouts: {
-				files: ['themes/franson/layouts/**/**.*'],
+				files: ['themes/<%= hugo_theme %>/layouts/**/**.*'],
+				tasks: ['exec'],
+				options: {
+					livereload: true
+				}
+			},
+			images: {
+				files: ['themes/<%= hugo_theme %>/static/assets/images/**/**.*'],
 				tasks: ['exec'],
 				options: {
 					livereload: true
@@ -66,10 +103,11 @@ module.exports = function (grunt) {
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-exec');
 	
-	grunt.registerTask('default', ['connect', 'watch']);
+	grunt.registerTask('default', ['less', 'concat', 'exec', 'connect', 'watch']);
 	
 };
